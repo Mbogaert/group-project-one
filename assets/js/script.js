a0d0de0e975bbbc80051388b7f4a52766adedc1e
Mathew B <bogaert@email.arizona.edu>
2020-11-06 13:17:43 -0700

    working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ var marsApiKey = "GrZ5XxlNbYcBVTrWcEVaRSagQlzo9TgtCbACgo64";
 var photoOneEl = document.querySelector("#photoOne");
 var photoTwoEl = document.querySelector("#photoTwo");
 
+// TBD 
+// display two photos - split the array?
+// randomize the display of the two photos
+
 
 // call Mars Photo API
 var getMarsPhotos = function(sol) {
@@ -19,9 +23,17 @@ var getMarsPhotos = function(sol) {
 var displayMarsPhotos = function(photoData) {
     console.log(photoData);
 
-    // clear old content
+    // clear old content - so that with each run of the application new images are generated
     photoOneEl.src = "";
     photoTwoEl.src = "";
+
+    // var randomNumber = Math.floor(Math.random() * 280);
+    // console.log(randomNumber);
+
+    var photoOne = photoData[0].img_src
+    console.log(photoOne);
+    // set photoOne and photoTwo name
+    // set photo container to display the mars image
 };
 
 getMarsPhotos();
