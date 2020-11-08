5e359b5e2d6f81321e1da09219a8dc7eb5978338
Mathew B <bogaert@email.arizona.edu>
2020-11-08 13:30:37 -0700

    small change

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,9 @@ var city = document.querySelector("#city");
 
 // change the displayed photo size or check to make sure displayed photo size is big enough
 // add another camera/API to the other photo
-// figure out what image should be displayed on landing on the page - if any
+// figure out what image should be displayed on landing on the page -
 // Maybe: do we want to see if we can match the sol day that is displayed in the Mars weather week to the sol day of the photo - waiting for Mars week to be added to explore
 
-<<<<<<< HEAD
-// call Mars Photo API for Opportunity Rover Pancam
-=======
 var getMarsWeather = function (data) {
     fetch("https://api.nasa.gov/insight_weather/?api_key=" + marsApiKey + "&feedtype=json&ver=1.0")
         .then(r => r.json())
@@ -46,8 +43,8 @@ var getMarsWeather = function (data) {
 
 }
 getMarsWeather();
-// call Mars Photo API
->>>>>>> main
+
+// // call Mars Photo API for Opportunity Rover Pancam
 var getMarsPhotos = function (sol) {
     var sol = 10 + Math.floor(Math.random() * (1000 - 10 + 1));
     var marsApiUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=" + sol + "&camera=pancam&api_key=" + marsApiKey;
